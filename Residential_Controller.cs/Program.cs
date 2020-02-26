e43d50aefef00950ea4d20abf9c1beea6bf4b763
mbeauchemin1 <60408314+mbeauchemin1@users.noreply.github.com>
2020-02-26 16:08:47 -0500

    new commit c#

---
+++ Residential_Controller.cs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Residential_Controller.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
